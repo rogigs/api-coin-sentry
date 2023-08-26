670b3645c43f937e14e0aad93c07402fbc76aaf1
Igor Soares <igorgomes.13ig@gmail.com>
2023-08-26 15:37:50 -0300

    chore: add swagger autogen

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const historicRoute = require("./routes/historicRoute");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("../swagger_output.json");
 
 app.use(express.json());
 app.use(cors());
@@ -9,6 +11,7 @@ app.use(cors());
 const PORT = process.env.PORT || 4000;
 
 app.use("/api", historicRoute);
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(PORT, () => {
   console.log(`Server on ${PORT}...`);
