be7459100abd728bd314c324c0e9abd2a24d6452
Igor Soares <igor.soares.developer@gmail.com>
2024-01-21 17:49:32 -0300

    fix: add env that vercel expected

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -8,11 +8,11 @@ dotenv.config();
 // TODO: make migration work
 const AppDataSource = new DataSource({
   type: "postgres",
-  port: +process.env.TYPEORM_PORT | 5432,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  host: process.env.TYPEORM_HOST,
+  port: 5432,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  host: process.env.PGHOST,
   synchronize: true,
   logging: true,
   migrations: [],
