b05f259d3e7fec4b392e3fc23850974e0fcf0670
Igor Soares <igor.soares.developer@gmail.com>
2024-01-21 17:31:16 -0300

    fix: change port of database

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -8,7 +8,7 @@ dotenv.config();
 // TODO: make migration work
 const AppDataSource = new DataSource({
   type: "postgres",
-  port: +process.env.TYPEORM_PORT,
+  port: +process.env.TYPEORM_PORT | 5432,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
